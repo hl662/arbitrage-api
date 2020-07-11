d0d400bea65ff5b1ce74a6eaa01f729cb2e81174
Hoang Nam Le <hl662@drexel.edu>
2020-07-11 00:47:40 -0400

    finished 1st draft of override toString

--- src/utils/DiGraph.java
+++ src/utils/DiGraph.java
@@ -1,29 +1,55 @@
+// https://www.cs.cornell.edu/courses/cs211/2006fa/Lectures/L22-More%20Graphs/Digraph.java
+package utils;
+
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+// Credits to Cornells' CS211 DiGraph Implementation, where I used it for reference.
 public class DiGraph {
-    // We want the weighted edge between two vertices. Use an adjacency list.
-    private Map<Vertex, List<Vertex>> neighbors = new HashMap<Vertex, List<Vertex>>();
+	// We want the weighted edge between two vertices. Use an adjacency list.
+	private Map<Vertex, List<Vertex>> neighbors = new HashMap<Vertex, List<Vertex>>();
 
-    public String toString() {
+	public String toString() {
+		StringBuffer result = new StringBuffer();
+		for (Vertex v : neighbors.keySet()) {
+			result.append(String.format("\n%s\n", v)); // Will later use the neighbors.get(v) to get the items neighbors.
+		}
+		return result.toString();
+	}
 
-        for (Vertex v : neighbors.keySet()) {
-            System.out.println(v); // Will later use the neighbors.get(v) to get the items neighbors.
-        }
-    }
-    public static void main(String[] args) {
+	public void add(Vertex newVertex) {
+		// Initialize a new adjacent list;
+		neighbors.put(newVertex, new ArrayList<Vertex>());
+	}
 
+	public static void main(String[] args) {
+      DiGraph testGraph = new DiGraph();
+			Vertex usd = new Vertex("USD");
+			Vertex yen = new Vertex("YEN");
+			Vertex gbp = new Vertex("GBP");
+			Vertex rand = new Vertex("RND");
+			testGraph.add(usd);
+			testGraph.add(yen);
+			testGraph.add(gbp);
+			testGraph.add(rand);
+			System.out.print(testGraph);
     }
 }
 
-// Initialize a weight between two vertices as infinity first, then assign the weight accordingly
+// Initialize a weight between two vertices as infinity first, then assign the
+// weight accordingly
 class Vertex {
-    // int weight;
-    String currency;
+	// int weight;
+	String currency;
 
-    @Override
-    public String toString() {
-        return String.format("Currency at this vertex: "+ currency + "\n");
-    }
+	public Vertex(String newCurrency) {
+		currency = newCurrency;
+	}
+
+	@Override
+	public String toString() {
+		return String.format("Currency at this vertex: " + currency);
+	}
 }
