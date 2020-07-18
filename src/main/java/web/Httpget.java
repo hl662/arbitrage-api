c7f13f10f6934ff2805249c3e9093cfa76ab3393
Hoang Nam Le <hl662@drexel.edu>
2020-07-17 20:58:00 -0400

    Updated pom.xml

--- src/main/java/web/Httpget.java
+++ src/main/java/web/Httpget.java
@@ -5,8 +5,8 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.URL;
 import javax.net.ssl.HttpsURLConnection;
-import org.json.JSONObject;
-import org.json.JSONArray;
+//import org.json.JSONObject;
+//import org.json.JSONArray;
 public class Httpget {
 
     // TODO: research java.org.JSONObject;
@@ -38,9 +38,9 @@ public class Httpget {
         }
     }
 
-    public void parseJSONData(String jsonString) {
-        JSONObject json = new JSONObject()
-    }
+//    public void parseJSONData(String jsonString) {
+//        JSONObject json = new JSONObject()
+//    }
 
     public static void main(String[] args) throws IOException{
         System.out.println(Httpget.fetchLatestRates());
