87406e9f9851d0c9adb26aaf80fa0d9c424a9551
Hoang Nam Le <hl662@drexel.edu>
2020-07-21 21:46:59 -0400

    Created json parser, hooked it with custom currency class. Created integration tests.

--- src/main/java/web/Httpget.java
+++ src/main/java/web/Httpget.java
@@ -7,16 +7,17 @@ import java.net.URL;
 import javax.net.ssl.HttpsURLConnection;
 import org.json.JSONObject;
 //import org.json.JSONArray;
-public class Httpget {
+public class HttpGet {
 
     // TODO: research java.org.JSONObject;
     public static final String baseExchangeRateURL = "https://api.exchangeratesapi.io/latest"; // By default, gets the base EUR.
     // TODO: Add a "?base={currentCurrency}" and do a for each in the currencies loop
     public static final String[] currencies = {"HRK", "CHF", "MXN", "ZAR", "INR", "THB", "CNY", "AUD", "ILS", "KRW", "JPY", "PLN", "GBP", "IDR", "HUF", "PHP", "TRY", "RUB", "HKD", "ISK", "DKK", "CAD", "USD", "MYR", "BGN", "NOK", "RON", "SGD", "CZK", "SEK", "NZD", "BRL", "GBP"};
     /**
-     *  Sends a HTTP Request GET to fetch the latest currency rates in string formatted JSON. 
+     *  Sends a HTTP Request GET to fetch the latest currency rates in string formatted JSON.
+     *  Uses the 'base' query param of the url to determine which
      */
-    public static String fetchLatestRates(String url) throws IOException {
+    public static String fetchCurrency(String url) throws IOException {
         URL getURL = new URL(url);
         HttpsURLConnection con = (HttpsURLConnection) getURL.openConnection();
         con.setRequestMethod("GET");
@@ -40,14 +41,9 @@ public class Httpget {
 
     public static void getAllLatestCurrencies() throws IOException {
         for (String currency : currencies) {
-            String url = Httpget.baseExchangeRateURL.concat(String.format("?base=%s", currency));
-            System.out.println(Httpget.fetchLatestRates(url));
+            String url = HttpGet.baseExchangeRateURL.concat(String.format("?base=%s", currency));
+            System.out.println(HttpGet.fetchCurrency(url));
         }
-    }
-
-    // Move to separate class file later.
-    public static JSONObject parseJSONData(String jsonString) {
-        return new JSONObject(jsonString);
     }
 
     public static void main(String[] args) throws IOException{
@@ -55,6 +51,6 @@ public class Httpget {
 //        JSONObject jsonObj = Httpget.parseJSONData(jsonString);
 //        JSONObject rates = (JSONObject) jsonObj.get("rates");
 //        System.out.println(rates.keySet());
-        Httpget.getAllLatestCurrencies();
+        HttpGet.getAllLatestCurrencies();
     }
 }
